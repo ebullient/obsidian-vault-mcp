8b74c80ebff540fc30de68bf5b6365658a19d630
Erin Schnabel <ebullientworks@gmail.com>
2026-07-29 15:36:27 -0400

    ♻️ remove unnecessary join method

--- src/vaultasmcp-Tools.ts
+++ src/vaultasmcp-Tools.ts
@@ -1013,29 +1013,7 @@ export class MCPTools {
         if (!filename.endsWith(".md")) {
             filename += ".md";
         }
-        return { path: normalizePath(this.join(folder, filename)) };
-    }
-
-    private join(...partSegments: string[]): string {
-        // Split the inputs into a list of path commands.
-        let parts: string[] = [];
-        for (let i = 0, l = partSegments.length; i < l; i++) {
-            parts = parts.concat(partSegments[i].split("/"));
-        }
-        // Interpret the path commands to get the new resolved path.
-        const newParts = [];
-        for (let i = 0, l = parts.length; i < l; i++) {
-            const part = parts[i];
-            // Remove leading and trailing slashes
-            // Also remove "." segments
-            if (!part || part === ".") continue;
-            // Push new path segments.
-            newParts.push(part);
-        }
-        // Preserve the initial slash if there was one.
-        if (parts[0] === "") newParts.unshift("");
-        // Turn back into a single string path.
-        return newParts.join("/");
+        return { path: normalizePath(`${folder}/${filename}`) };
     }
 
     /**
